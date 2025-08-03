b676ff0d29c01f5e460e39c6deee7f12cc3ae5c6
19161302 <19161302@itoaxaca.edu.mx>
2025-08-03 12:23:58 -0600

    actualizacion archivo app.js

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let arrayAmigos = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+    if(amigo !== '') {
+        arrayAmigos.push(amigo);
+        mostrarAmigos();
+        limpiarInput();
+    }else {
+        alert('Por favor, inserte un nombre');
+    }
+}
+
+function mostrarAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML =  ' ';
+    for(let i = 0; i < arrayAmigos.length; i++){
+        let li = document.createElement('li');
+        li.innerHTML = arrayAmigos[i];
+        lista.appendChild(li);
+    }
+    
+}
+
+function sortearAmigo(){
+    if(arrayAmigos.length > 0) {
+        let indiceAmigo = Math.floor(Math.random()* arrayAmigos.length);
+        let amigoSorteado = arrayAmigos[indiceAmigo];
+        reiniciarLista();
+        mostrarResultado(amigoSorteado);
+    } else {
+        alert("No hay amigos para sortear");
+    }
+}
+
+function mostrarResultado(amigoSorteado) {
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = 'El amigo secreto es: ' + amigoSorteado;
+    console.log('El amigo secreto es: ' + amigoSorteado);
+
+}
+
+function reiniciarLista() {
+    arrayAmigos = [];
+    document.getElementById('listaAmigos').innerHTML = '';
+}
+
+function limpiarInput(){
+    document.getElementById('amigo').value = '';
+}
+
